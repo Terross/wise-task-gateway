fe0415f0d2a4662747932b23919e83a32a97b119
magofrays <vadimpozdeev2005@gmail.com>
2025-11-17 12:32:04 +0300

    init.sh fix

--- init.sh
+++ init.sh
@@ -7,6 +7,15 @@ git clone git@github.com:Terross/wise-task-graph.git
 git clone git@github.com:Terross/wise-task-plugin.git
 
 cd wise-task-gateway
+cd src/main/resources
+mkdir certs
+cd certs
+
+openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048 # приватный ключ
+openssl rsa -in private.pem -pubout -out public.pem # публичный ключ
+
+cd ../../../..
+
 username="Terross"
 
 echo "Введите токен:"
