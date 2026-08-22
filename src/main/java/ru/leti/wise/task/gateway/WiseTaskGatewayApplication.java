de003e47a360b839c6ec298919e1c2b05f22b223
Vadim Pozdeev <vadimpozdeev2005@gmail.com>
2026-08-22 13:13:08 +0300

    remove useless dependencies + change version

--- src/main/java/ru/leti/wise/task/gateway/WiseTaskGatewayApplication.java
+++ src/main/java/ru/leti/wise/task/gateway/WiseTaskGatewayApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.grpc.client.ImportGrpcClients;
-import ru.leti.wise.task.graph.GraphServiceGrpc;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @ImportGrpcClients()
 public class WiseTaskGatewayApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(WiseTaskGatewayApplication.class, args);
     }
 
